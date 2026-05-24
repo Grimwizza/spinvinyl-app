ce0bdbc131a04918b049cf52c1a17b486ac13bdb
Grimwizza <ben.luebbert@gmail.com>
2026-05-24 11:33:47 -0500

    refactor: conditionally load apiMiddleware plugin only during development server execution

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import { apiMiddleware } from './vite-api-proxy.js'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(async ({ mode, command }) => {
   const env = loadEnv(mode, process.cwd(), '');
   Object.assign(process.env, env);
 
+  const plugins = [react()];
+  if (command === 'serve') {
+    const { apiMiddleware } = await import('./vite-api-proxy.js');
+    plugins.push(apiMiddleware());
+  }
+
   return {
-    plugins: [react(), apiMiddleware()],
+    plugins,
     server: {
       watch: {
         usePolling: true,
